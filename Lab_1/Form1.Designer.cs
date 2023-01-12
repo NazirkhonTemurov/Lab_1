eb920eb6021e0b12c1b92decac93468759ae83b1
Nazirkhon <temurovnazirkhon@gmail.com>
2023-01-12 16:16:10 +0700

    Оптимизировал код

---
+++ Lab_1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.рассчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отчиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(61, 171);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(61, 218);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "e";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "f(x)";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(786, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.рассчитатьToolStripMenuItem,
+            this.отчиститьToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // рассчитатьToolStripMenuItem
+            // 
+            this.рассчитатьToolStripMenuItem.Name = "рассчитатьToolStripMenuItem";
+            this.рассчитатьToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.рассчитатьToolStripMenuItem.Text = "Рассчитать ";
+            this.рассчитатьToolStripMenuItem.Click += new System.EventHandler(this.рассчитатьToolStripMenuItem_Click);
+            // 
+            // отчиститьToolStripMenuItem
+            // 
+            this.отчиститьToolStripMenuItem.Name = "отчиститьToolStripMenuItem";
+            this.отчиститьToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.отчиститьToolStripMenuItem.Text = "Отчистить";
+            this.отчиститьToolStripMenuItem.Click += new System.EventHandler(this.отчиститьToolStripMenuItem_Click);
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(310, 41);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(400, 300);
+            this.formsPlot1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 468);
+            this.Controls.Add(this.formsPlot1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "График Функций";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem рассчитатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отчиститьToolStripMenuItem;
+        private ScottPlot.FormsPlot formsPlot1;
+    }
+}
+
---
+++ Lab_1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScottPlot;
+
+namespace Lab_1
+{
+    public partial class Form1 : Form
+    {
+        private List<double> x = new List<double>();
+        private List<double> y = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formsPlot1.Plot.Title("f(x)");
+        }
+
+        private void name_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void рассчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double.TryParse(textBox1.Text, out var StartPoint);
+            double.TryParse(textBox2.Text, out var EndPoint);
+            double.TryParse(textBox3.Text, out var Accuracy);
+            double a = StartPoint;
+            double b = EndPoint;
+            double x1, x2;
+            double Middle = 0, Result;
+
+            //метод чтобы найти точки минимума
+            double IncrementStep = Accuracy / 10;
+
+            int count = 0;
+            /*try
+            {*/
+            while (true)
+            {
+                ++count;
+
+                x1 = (StartPoint + EndPoint - IncrementStep) / 2;
+                x2 = (StartPoint + EndPoint + IncrementStep) / 2;
+                try
+                {
+                    if (F(x1) <= F(x2))
+                        EndPoint = x2;
+                    else
+                        StartPoint = x1;
+
+                    Middle = (EndPoint - StartPoint) / 2;
+
+                    if (Middle <= Accuracy)
+                    {
+                        Result = (StartPoint + EndPoint) / 2;
+                        formsPlot1.Plot.Clear();
+                        formsPlot1.Refresh();
+                        Show(a, b, Result);
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                    break; ;
+                }
+            }
+
+        }
+        
+
+        private double F(double X)
+        {
+            org.matheval.Expression expression = new org.matheval.Expression(textBox4.Text.ToLower());
+            expression.Bind("x", X);
+            decimal value = expression.Eval<decimal>();
+            /*System.Windows.Forms.MessageBox.Show(value.ToString());*/
+            return (double)value;
+        }
+
+        private void отчиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Show(double StartPoint, double EndPoint, double Result)
+        {
+            double xs = Result;
+            double ys = F(Result);
+            for (double i = StartPoint; i <= EndPoint; i += 0.1)
+            {
+                x.Add(i);
+                y.Add(F(i));
+            }
+            formsPlot1.Plot.Title($"Точка минимум - min[{Math.Round(xs, 5)} ; {Math.Round(ys, 5)}]");
+            formsPlot1.Plot.AddScatter(x.ToArray(), y.ToArray(), markerShape: MarkerShape.none, lineWidth: 3);
+            /*formsPlot1.Plot.AddScatter(
+                new double[] { xs },
+                new double[] { ys },
+                color: System.Drawing.Color.FromName("Green"),
+                markerSize: 7);
+            formsPlot1.Plot.AddArrow(xs,
+                 ys, xs - 3, ys,
+                color: System.Drawing.Color.FromName("Red"));*/
+            formsPlot1.Refresh();
+
+
+        }
+
+    }
+}
